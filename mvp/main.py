0e0dff24f5e53bdd0f00905193921b56cc322d24
GeorgeFane <georgefane11@gmail.com>
2021-03-05 05:02:00 +0000

    commented up the code

--- mvp/main.py
+++ mvp/main.py
@@ -7,14 +7,21 @@ from swagger import *
 
 app = Flask(__name__)
 
+# stores cred ids for each user
+# faux multi-wallet system
 subwallet = {}
+
+# creates new cred format named ssn
+# with attrs: ssn, issuer, holder, timestamp
 register('ssn')
 
+# for presenting creds and getting the FSP to issue you creds
 @app.route('/user/<user>', methods=['POST', 'GET'])
 def index(user):
     global subwallet
     print(subwallet)
 
+    # creates storage for user if nonexistent yet
     if user not in subwallet:
         subwallet[user] = []
 
@@ -25,23 +32,34 @@ def index(user):
         if data.get('dtype') and data.get('info'):
             issue(data['dtype'], data['info'], user)
 
+            # get all credentials across users (not good)
             creds = requests.get(
                 admin['alice'] + '/credentials'
             ).json()
 
-            referent = sorted(
+            # sort by timestamp, most recent first
+            sortedCreds = sorted(
                 creds['results'],
                 key = lambda x: int(x['attrs']['timestamp']),
                 reverse=True
-            )[0]['referent']
+            )
+
+            # get id of most recent cred
+            referent = sortedCreds[0]['referent']
+
+            # store id as one of user's
             subwallet[user].append(referent)
         
         # request proof
         # check box to offer cred
+
+        # gets all selected cred types
         dtypes = data.getlist('token')
         if dtypes:
             present(dtypes)
-        
+    
+    # gets creds by id one-by-one
+    # looks at the user's stored ids
     results = [
         requests.get(
             admin['alice'] + '/credential/' + cred_id
@@ -49,37 +67,50 @@ def index(user):
         for cred_id in subwallet[user]
     ]
 
+    # set() function to avoid repeats
+    # 'cred_def_id' is something long like HcnMNFeUxjX1AzpDuP7ZzD:3:CL:52015:ssn
+    # so split by colon to get 'ssn' (cred name) at the end
     dtypes = set([
         cred['cred_def_id'].split(':')[-1]
         for cred in results
     ])
-        
+
+    # pass dtypes (creds that user already has)
+    # to show up in user's cred list  
     return render_template(
         'index.html', 
         dtypes=dtypes
     )
 
+# gets ids of all creds across users and deletes by id one-by-one
 @app.route('/delete', methods=['POST', 'GET'])
 def delete():
+    # get all creds
     credentials = requests.get(
         admin['alice'] + '/credentials'
     ).json()['results']
 
+    # delete by id aka referent
     for cred in credentials:
         requests.delete(
             admin['alice'] + '/credential/' + cred['referent']
         )
 
+# for forming connections between agents
 @app.route('/', methods = ['GET', 'POST'])
 def conn():
     if request.method == 'POST':
         data = request.form
 
+        # Alice agent stores invitation cred in wallet,
+        # activates it, and sends corresponding invitation to Faber
         requests.post(
             admin['alice'] + '/connections/receive-invitation',
             data=data['invitation']
         )
 
+        # redirect to user page of whatever name user submitted
+        # no actual secure login system yet
         return redirect(
             url_for(
                 'index',
@@ -88,9 +119,12 @@ def conn():
         )
 
     else:
+        # Faber creates invitation json
         inv = requests.post(
             admin['faber'] + '/connections/create-invitation'
         ).json()
+
+        # formatting
         dumped = json.dumps(
             inv['invitation'],
             indent=4
--- mvp/swagger.py
+++ mvp/swagger.py
@@ -3,22 +3,32 @@ import json
 from pprint import pprint
 import time
 
+# admin API routes
 admin = {
     'faber': 'http://0.0.0.0:8021',
     'alice': 'http://0.0.0.0:8031'
 }
 
+# get Faber DID
 issuer_did = requests.get(
     admin['faber'] + '/wallet/did/public'
 ).json()['result']['did']
 
+# get list of ids of cred defs that Faber made
+# should be 2 things: default education cred from Aries and ssn
 definitions = requests.get(
     admin['faber'] + '/credential-definitions/created'
 ).json()['credential_definition_ids']
 
+# dumb way to get cred names
+# ids look like HcnMNFeUxjX1AzpDuP7ZzD:3:CL:52015:ssn
+# so split by colon and get last thing
 keys = [
     s.split(':')[-1] for s in definitions
 ]
+
+# easy access cred def ids
+# e.g. 'ssn': 'HcnMNFeUxjX1AzpDuP7ZzD:3:CL:52015:ssn'
 pairs = dict(
     zip(
         keys, definitions
@@ -26,9 +36,11 @@ pairs = dict(
 )
 
 def register(dtype):
+    # avoid registering same def twice
     if dtype in keys:
         return
 
+    # create schema
     schema_body = {
         "schema_name": dtype,
         "schema_version": '1.1.1',
@@ -39,6 +51,7 @@ def register(dtype):
         json=schema_body
     ).json()
 
+    # send to ledger (actually activate)
     schema_id = schema_response["schema_id"]
     credential_definition_body = {
         "schema_id": schema_id,
@@ -49,15 +62,20 @@ def register(dtype):
         json=credential_definition_body
     ).json()
 
+    # returned vals not stored, but Aries sample does this
     cred_def_id = cred_def_response["credential_definition_id"]   
     return schema_id, cred_def_id
 
 def issue(dtype, info, holder):
     cred_def_id = pairs[dtype]
+
+    # gets Faber's connection id to Alice
+    # should by 0th id (most recent)
     connection_id = requests.get(
         admin['faber'] + '/connections'
     ).json()['results'][0]['connection_id']
 
+    # set attrs to pass
     cred_attrs = {
         "holder": holder,
         "issuer": issuer_did,
@@ -65,6 +83,8 @@ def issue(dtype, info, holder):
         'timestamp': str(int(time.time()))
     }
 
+    # for each key: value pair, becomes 
+    # 'name': key, 'value': value
     cred_preview = {
         "@type": "https://didcomm.org/issue-credential/2.0/credential-preview",
         "attributes": [
@@ -73,6 +93,7 @@ def issue(dtype, info, holder):
         ],
     }
     
+    # final package, contains everything we built
     offer_request = {
         "connection_id": connection_id,
         "comment": f"Offer on cred def id {cred_def_id}",
@@ -82,6 +103,8 @@ def issue(dtype, info, holder):
         "trace": False,
     }
 
+    # returned value not used
+    # should check if this contains cred id, to store in user's subwallet
     return requests.post(
         admin['faber'] + "/issue-credential-2.0/send-offer", 
         json=offer_request
@@ -89,10 +112,15 @@ def issue(dtype, info, holder):
 
 def present(dtypes: list):
     req_attrs = [{'name': dtype} for dtype in dtypes]
+
+    # gets Faber's connection id to Alice
+    # should by 0th id (most recent)
     connection_id = requests.get(
         admin['faber'] + '/connections'
     ).json()['results'][0]['connection_id']
 
+    # converts 'ssn', 'address' to '0_ssn_uuid', '0_address_uuid'
+    # weird, but cred attrs are stored that way in the metadata
     indy_proof_request = {
         "name": "Proof of KYC",
         "version": "1.0",
